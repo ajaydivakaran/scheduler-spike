3e94820a09ca4fa61683c5a9460fca33d0fe6380
Ajay Divakaran <ajay_886@hotmail.com>
2017-04-12 16:30:05 +0530

    Sample app using quartz approach

--- src/main/java/com/spike/QuartzSpikeApplication.java
+++ src/main/java/com/spike/QuartzSpikeApplication.java
@@ -2,18 +2,18 @@ package com.spike;
 
 import com.spike.config.AutowiringSpringBeanJobFactory;
 import com.spike.job.SampleJob;
-import org.quartz.JobDetail;
-import org.quartz.SimpleTrigger;
-import org.quartz.Trigger;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+import java.io.IOException;
+
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 
 @SpringBootApplication
 public class QuartzSpikeApplication {
@@ -25,32 +25,34 @@ public class QuartzSpikeApplication {
 	}
 
 	@Bean
-	public JobDetailFactoryBean jobDetail() {
-		JobDetailFactoryBean jobDetailFactory = new JobDetailFactoryBean();
-		jobDetailFactory.setJobClass(SampleJob.class);
-		jobDetailFactory.setDescription("Invoke Sample Job service...");
-		jobDetailFactory.setDurability(true);
-		return jobDetailFactory;
+	public JobDetail jobDetail() {
+		return JobBuilder.newJob().ofType(SampleJob.class)
+				.storeDurably()
+				.withIdentity("Qrtz_Job_Detail")
+				.withDescription("Invoke Sample Job service...")
+				.build();
 	}
 
 	@Bean
-	public SimpleTriggerFactoryBean trigger(JobDetail job) {
-		SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
-		trigger.setJobDetail(job);
-		trigger.setRepeatInterval(360);
-		trigger.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
-		return trigger;
+	public Trigger trigger(JobDetail job) {
+		return TriggerBuilder.newTrigger().forJob(job)
+				.withIdentity("Qrtz_Trigger")
+				.withDescription("Sample trigger")
+				.withSchedule(simpleSchedule().repeatForever().withIntervalInSeconds(10))
+				.build();
 	}
 
 	@Bean
-	public SchedulerFactoryBean scheduler(Trigger trigger, JobDetail job) {
-		SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
-		schedulerFactory.setConfigLocation(new ClassPathResource("quartz.properties"));
+	public Scheduler scheduler(Trigger trigger, JobDetail job) throws SchedulerException, IOException {
+		StdSchedulerFactory factory = new StdSchedulerFactory();
+		factory.initialize(new ClassPathResource("quartz.properties").getInputStream());
 
-		schedulerFactory.setJobFactory(springBeanJobFactory());
-		schedulerFactory.setJobDetails(job);
-		schedulerFactory.setTriggers(trigger);
-		return schedulerFactory;
+		Scheduler scheduler = factory.getScheduler();
+		scheduler.setJobFactory(springBeanJobFactory());
+		scheduler.scheduleJob(job, trigger);
+
+		scheduler.start();
+		return scheduler;
 	}
 
 	@Bean
